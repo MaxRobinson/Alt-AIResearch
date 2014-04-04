cf91c4d216fc38cc824e0290c0cf5d4400ff6825
Max Robinson <maxrgit@gmail.com>
2014-04-04 16:19:01 -0700

    Added Test_smartAgent Class
    We have added a class for JUnit testing of the SmartAgent Class that way we
    can insure that all of our functions are working individually.

---
+++ Test_smartAgent.java
@@ -0,0 +1,14 @@
+package randomPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test_smartAgent {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
