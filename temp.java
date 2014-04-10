483e3aebcbd59368971281c534f457c1c408e910
MaxRobinson <maxrgit@gmail.com>
2014-04-10 14:11:06 -0700

    AnalyzeMove Revision 1 Done
    This commit has a new version of AnalyzeMove in the temp.java file. I have
    not added anything to smartAgent.java yet becuase revisions still may have
    to be made. This Branch needs to be merged with the Refactor Branch.

--- temp.java
+++ temp.java
@@ -43,20 +43,43 @@ public class temp {
         
         //could be in with the loop in another method? 
         ArrayList<Integer> indexListTemp = new ArrayList<Integer>();
-        //? boolean breakFlag = false;
-        
         // * narrowMatches() * ==> to match the next spot.
-        
-        
         //should this be it's own method? 
         while(indexList.size() > 1){
+            
+            //The State we are matching needs to be changing and it is not...
+            // need to do something like stateToMatch = currentPath.get(currentMatchedPathLength);
+            stateToMatch = currentPath.get(currentMatchedPathLength);
+            
             indexListTemp = indexList; //This is a safe guard in case no matches are found. 
-            indexList = narrowMatches(episodicMemory, indexList);
+            indexList = narrowMatches(episodicMemory, indexList,stateToMatch); //then pass in here <<stateToMatch
+            //Makes sure that indexList length is not 0, it it is, 
+            //go back to the last list that had something in it and break
             if(indexList.size() <= 0){
                 indexList = indexListTemp;
                 break;
             }
+            currentMatchedPathLength++; //we can do this because we know the indexList is not 0;
         }//while
+        
+        
+    // HERE We have a list of Indices that go as far back as the currentMatchedPathLength from the
+    //  original state we wanted to match.
+        int index = indexList.get(indexList.size()-1) + currentMatchedPathLength; //gets back to the episode we want
+        
+        //build Conjecture Path
+        conjecturePath = buildConjecturePath(index);
+        
+        //test conjectured Path
+        ListAndBool newPath = testConjecture(conjecturePath);
+        boolean areSame = newPath.getReturnValue(); //checks if the episodes were found to be the same
+                                                    //may not matter
+        if(newPath.getConjecturePath() != null){
+            transitionTable.addPath(newPath.getConjecturePath()); //addPath to transTable May not want to do here.
+        }
+        
+        return newPath;
+
     }//analyzeMove();
     
     
@@ -66,14 +89,17 @@ public class temp {
      * @param indexList
      * @return
      */
-    public ArrayList<Integer> narrowMatches(ArrayList<Episode>listOfEpisodes, ArrayList<Integer> indexList){
+    public ArrayList<Integer> narrowMatches(ArrayList<Episode>listOfEpisodes, ArrayList<Integer> indexList, Episode stateToMatch){
         if(indexList.size() == 1){
             return indexList;
         }
+        //decriment all of the indices in the index list by 1
+        ArrayList<Integer> newIndexList = decrementArrayList(indexList,1);
+        //check if episode has occurred.
+        ArrayList<Integer> resultIndexList = checkIfEpisodeOccured(newIndexList,stateToMatch);
         
         
-        
-        return indexList; //Place Holder
+        return resultIndexList; //Place Holder
     }//NarrowMatches();
     
     
